0690236e7bd35a22da9a41140859af24bd05236d
Conrad <Conrad.mayer72@gmail.com>
2023-08-28 15:34:11 -0500

    Implementing SDL error return / proper trown errors

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <stdexcept>
 
 int main(){
     if (SDL_Init(SDL_INIT_VIDEO) != 0){
-        std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
-        return 1;
+        throw std::runtime_error(
+            std::string("Could not INIT SDL2: ")
+            + SDL_GetError()
+        );
     }
     SDL_Quit();
     return 0;
