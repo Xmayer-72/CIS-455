81067e81fd439a2e799cb56879e485da61306137
Conrad <Conrad.mayer72@gmail.com>
2023-08-28 16:45:56 -0500

    Final 8/28 Work

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,11 @@
 
 int main(){
     CanvasBase Canvas("", 300, 300);
-while (should_keep_rendering)
+while (should_keep_rendering())
 {
     Canvas.clear();
 
-    Canvas.put_pixel({0,0},Color::red);
+    Canvas.put_pixel({0,0}, Color::red);
 
     Canvas.present();
 }
