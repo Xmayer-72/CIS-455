e58efb34c71ed3afcf273f2a3e5e4ac2512568ac
Conrad <Conrad.mayer72@gmail.com>
2023-09-04 15:29:07 -0500

    Cleanup

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,14 +2,14 @@
 #include "Misc.h"
 
 int main(){
-    Canvas Canvas("", 600, 600);
+    Canvas Canvas("", 650, 650);
     while (should_keep_rendering())
     {
         Canvas.clear();
 
         Canvas.draw_line_2d({-120, -240}, {120, 240}, Color::aquamarine);
 
-        Canvas.draw_line_2d({240, 120},{-200,-100}, Color::red_violet);
+        Canvas.draw_line_2d({240, 120},{-200,-10}, Color::red_violet);
 
         Canvas.draw_line_2d({-50,-200},{60,240}, Color::gold);
 
