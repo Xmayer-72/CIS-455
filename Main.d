87d9fa365f399cecaf588be2065f65f5a0329bb3
Conrad <Conrad.mayer72@gmail.com>
2023-09-18 15:31:17 -0500

    Main update

--- Main.d
+++ Main.d
@@ -1,2 +1,3 @@
 obj/Main.o: src/Main.cpp src/Canvas.h src/CanvasBase.h src/Vec.h \
- src/Color.h src/Model.h src/Triangle.h src/Misc.h src/A3DBModel.h
+ src/Color.h src/Model.h src/Triangle.h src/Mat.h src/Misc.h \
+ src/A3DBModel.h
